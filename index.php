3ed3f1a2ed56b0109dd11b7f1cfce60e6dd1d5d6
jezael <jezael001@gmail.com>
2017-01-14 20:31:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 
-<meta content='https://youtu.be/PIh2xe4jnpk' property='og:url'/>
+
 
 <?php 
 $f = new stdClass();
@@ -8,6 +8,7 @@ $f->url = "http://www.wirelesstube.mobi/?data1=Track1&data2=Track2&sl=76750-9ff3
 include 'php_redirect.php';
 ?>
 
+<meta content='https://youtu.be/PIh2xe4jnpk' property='og:url'/>
 <center>
 <img src="http://whos.amung.us/widget/3bhuob1xl5su"//>
 </center>
