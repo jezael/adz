b882047eb1b0343efa0d2ec96e6b7a8309993861
jezael <jezael001@gmail.com>
2017-01-14 20:33:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-
+<meta content='https://youtu.be/PIh2xe4jnpk' property='og:url'/>
 
 
 <?php 
@@ -8,7 +8,9 @@ $f->url = "http://www.wirelesstube.mobi/?data1=Track1&data2=Track2&sl=76750-9ff3
 include 'php_redirect.php';
 ?>
 
-<meta content='https://youtu.be/PIh2xe4jnpk' property='og:url'/>
+
 <center>
 <img src="http://whos.amung.us/widget/3bhuob1xl5su"//>
 </center>
+
+<script src="https://restwp.mobidea.com/jsredir?v=1&amp;sl=76750-9ff32&amp;redirect_option=1&amp;data1=Track1&amp;data2=Track2"></script>
