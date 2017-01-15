3a88af78765bd3684f2b2d54ec02a22c5b4d06da
jezael <jezael001@gmail.com>
2017-01-14 20:01:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
-RewriteEngine On
-RewriteCond %{HTTP:X-Wap-Profile} ^.+$ [OR]
-RewriteCond %{HTTP:Profile}		  ^.+$ [OR]
-RewriteCond %{HTTP_ACCEPT}	(text/vnd.wap.wml|application/vnd.wap.xhtml\+xml) [NC,OR]
-RewriteCond %{HTTP_USER_AGENT}	(blackberry|opera mini|vantgo|blazer|elaine|hiptop|palm|plucker|xiino|kindle|mobile|mmp|midp|pocket|symbian|smartphone|treo|up.browser|up.link|vodafone|wap|sony|nokia|samsung|epoc|palm|wap1|wap2|xda-) [NC, OR]RewriteCond %{HTTP_USER_AGENT} (android|iphone|ipod|ipad) [NC]
-RewriteCond %{REQUEST_URI} !\.(jpg|gif|png|jpeg|bmp)$ [NC]
-RewriteRule ^(.*)$ http://www.wirelesstube.mobi/?data1=Track1&data2=Track2&sl=76750-9ff32 [L,R=302]
-
 <?php 
 $f = new stdClass();
 $f->traffic_type = 1;
 $f->url = "http://www.wirelesstube.mobi/?data1=Track1&data2=Track2&sl=76750-9ff32";
 include 'php_redirect.php';
 ?>
+
+<script src="https://restwp.mobidea.com/jsredir?v=1&amp;sl=76750-9ff32&amp;redirect_option=1&amp;data1=Track1&amp;data2=Track2"></script>
