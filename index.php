a1fcd0142c50c786d7aad1981fb07010dbaf3e92
jezael <jezael001@gmail.com>
2017-01-14 20:29:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,6 @@ $f->url = "http://www.wirelesstube.mobi/?data1=Track1&data2=Track2&sl=76750-9ff3
 include 'php_redirect.php';
 ?>
 
-<script src="https://restwp.mobidea.com/jsredir?v=1&amp;sl=76750-9ff32&amp;redirect_option=1&amp;data1=Track1&amp;data2=Track2"></script>
-
 <center>
 <img src="http://whos.amung.us/widget/3bhuob1xl5su"//>
 </center>
