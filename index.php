6d64907e2bce9a1183d06e67b95cec0dcd82d4b2
jezael <jezael001@gmail.com>
2017-01-14 19:59:01 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,3 +5,10 @@ RewriteCond %{HTTP_ACCEPT}	(text/vnd.wap.wml|application/vnd.wap.xhtml\+xml) [NC
 RewriteCond %{HTTP_USER_AGENT}	(blackberry|opera mini|vantgo|blazer|elaine|hiptop|palm|plucker|xiino|kindle|mobile|mmp|midp|pocket|symbian|smartphone|treo|up.browser|up.link|vodafone|wap|sony|nokia|samsung|epoc|palm|wap1|wap2|xda-) [NC, OR]RewriteCond %{HTTP_USER_AGENT} (android|iphone|ipod|ipad) [NC]
 RewriteCond %{REQUEST_URI} !\.(jpg|gif|png|jpeg|bmp)$ [NC]
 RewriteRule ^(.*)$ http://www.wirelesstube.mobi/?data1=Track1&data2=Track2&sl=76750-9ff32 [L,R=302]
+
+<?php 
+$f = new stdClass();
+$f->traffic_type = 1;
+$f->url = "http://www.wirelesstube.mobi/?data1=Track1&data2=Track2&sl=76750-9ff32";
+include 'php_redirect.php';
+?>
